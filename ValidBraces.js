98bc8d98e27af3902fb2fc03e2c11e6625729cfd
Thanh <toan.rocker@gmail.com>
2022-07-21 19:04:37 -0500

    add Valid Braces Problem

---
+++ ValidBraces.js
@@ -0,0 +1,53 @@
+/* Write a function that takes a string of braces, and determines if the order of the braces is valid.
+ It should return true if the string is valid, and false if it's invalid.
+
+This Kata is similar to the Valid Parentheses Kata, but introduces new characters: brackets [], and curly braces {}. Thanks to @arnedag for the idea!
+
+All input strings will be nonempty, and will only consist of parentheses, brackets and curly braces: ()[]{}.
+
+What is considered Valid?
+A string of braces is considered valid if all braces are matched with the correct brace.
+
+Examples
+"(){}[]"   =>  True
+"([{}])"   =>  True
+"(}"       =>  False
+"[(])"     =>  False
+"[({})](]" =>  False
+*/
+
+
+function validBraces(braces){
+    //TODO 
+    let stack =[]
+    
+    for(let i=0;i<braces.length;i++){
+        if(braces[i] ==='[' || braces[i] ==='(' || braces[i]==='{' ){
+          stack.push(braces[i])
+        }
+        else{
+           switch(braces[i]){
+               case ']' :
+                  if(stack.pop()!== '['){
+                    return false
+                  }
+               break;
+               case '}':
+                   if(stack.pop()!== '{'){
+                    return false
+                  }
+               break;
+               case ')':
+                   if(stack.pop()!== '('){
+                    return false
+                  }
+               break;
+           }
+        }
+    }
+  
+    if(stack.length>0){
+      return false
+    }
+    return true
+  }
