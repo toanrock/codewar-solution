51063fd9c5e18a5be679dca891bf6d9e31ba3cfe
Thanh <toan.rocker@gmail.com>
2022-07-27 23:40:55 -0500

    change to es6 arrow function

--- UniqueInOrder.js
+++ UniqueInOrder.js
@@ -7,14 +7,16 @@ uniqueInOrder('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D', 'A', 'B']
 uniqueInOrder('ABBCcAD')         == ['A', 'B', 'C', 'c', 'A', 'D']
 uniqueInOrder([1,2,2,3,3])       == [1,2,3]
 */
-var uniqueInOrder=function(iterable){
+const uniqueInOrder = (iterable) => {
   //your code here - remember iterable can be a string or an array
-   let arrString = Array.isArray(iterable)? iterable: iterable.split('');
-  
-  return arrString.reduce((preValue,curValue)=>{
-    if(preValue[preValue.length-1]!==curValue){
+  let arrString = Array.isArray(iterable) ? iterable : iterable.split('');
+
+  return arrString.reduce((preValue, curValue) => {
+
+    if (preValue[preValue.length - 1] !== curValue) {
       preValue.push(curValue)
     }
     return preValue
-  },[])
+  }, [])
+
 }
